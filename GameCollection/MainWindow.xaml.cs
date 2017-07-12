937b69efcbb954c5613220e092dabb21254c5e96
Dave Arneson <urvaius@gmail.com>
2017-07-12 15:48:22 -0700

    basics working for now

--- GameCollection/MainWindow.xaml.cs
+++ GameCollection/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Oracle.DataAccess.Client;
 
 namespace GameCollection
 {
@@ -23,6 +24,38 @@ namespace GameCollection
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string test = "notthere";
+
+            
+
+            string oradbnew = "Data Source=(DESCRIPTION="
+             + "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.1.5.250)(PORT=1521)))"
+             + "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=gamecollecti)));"
+             + "User Id=game;Password=Buffy11$;";
+
+            string SelectGames = "select * from games";
+
+            OracleConnection conn = new OracleConnection(oradbnew);
+            conn.Open();
+            Console.WriteLine("connected to oracle" + conn.ServerVersion);
+            test = conn.InstanceName.ToString();
+
+
+            OracleCommand cmd = new OracleCommand(SelectGames, conn);
+            cmd.CommandType = System.Data.CommandType.Text;
+
+            OracleDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            test = dr["NAME"].ToString();
+            
+            conn.Close();
+            conn.Dispose();
+            Console.WriteLine("disconnected");
+
         }
     }
 }
